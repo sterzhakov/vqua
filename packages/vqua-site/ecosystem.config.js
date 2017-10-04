f9b8cf7148188299096066054d79399aaad0b2b3
Igor <sterjakov@icloud.com>
2017-10-04 22:03:30 +0300

    pm2 config file

---
+++ packages/vqua-site/ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+  apps : [
+    {
+      name      : 'Vqua site',
+      script    : 'packages/vqua-site/server/index.js'
+    }
+  ],
+
+  deploy : {
+    production : {
+      user : 'sterjakov',
+      host : '194.67.194.137',
+      ref  : 'origin/master',
+      repo : 'https://github.com/sterjakovigor/vqua',
+      path : '/home/sterjakov/vqua-site',
+      'post-deploy' : [
+        'npm install',
+        'pm2 reload ecosystem.config.js --env production'
+      ].join(' && ')
+    },
+  }
+};
