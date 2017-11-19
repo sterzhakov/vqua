6bf3671b67ecdae15b8e90df1b43a9d8498e70dc
Igor <sterjakov@icloud.com>
2017-11-19 23:42:22 +0300

    readme

--- packages/vqua-boilerplate-ssr/dist/index.js
+++ packages/vqua-boilerplate-ssr/dist/index.js
@@ -4584,8 +4584,8 @@ class Navigation {
         send: (statusCode, componentName, params = {}) => {
           callback({ statusCode, componentName, params })
         },
-        redirect: (statusCode, redirectPath, params = {}) => {
-          callback({ statusCode, redirectPath, params })
+        redirect: (statusCode, path, params = {}) => {
+          callback({ statusCode, path, params })
         }
       }
 
